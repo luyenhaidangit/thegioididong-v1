7ac29f7d6a351a5d9c57cf7771af854d1c7e0deb
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-08 23:06:27 +0700

    Add data bigcampaign slide

--- Thegioididong.Common/SQL/Data/Slide.sql
+++ Thegioididong.Common/SQL/Data/Slide.sql
@@ -3,3 +3,23 @@ VALUES (N'Header Top Bar Slide', 'client', 'header_top', 1);
 
 INSERT INTO SlideItems(SlideId, Title, Image, URL)
 VALUES (11, NULL, '/Upload/Image/Slide/header-top-slide-1.png', NULL);
+
+INSERT INTO Slides(Name, Page, Position, Published)
+VALUES (N'Big Campaign Slide', 'home', 'bigcampaign', 1);
+
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-1.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-2.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-3.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-4.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-5.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-6.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-7.png', NULL);
+INSERT INTO SlideItems(SlideId, Title, Image, URL)
+VALUES (13, NULL, '/Upload/Image/Slide/big-campaign-slide-8.png', NULL);

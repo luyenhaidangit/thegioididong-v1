c8f6599400eeec6956bb187e3772c95ac93dc103
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-10 01:41:26 +0700

    Delete project web

--- Thegioididong.Web/ClientApp/src/data/BannerTopzone.js
+++
@@ -1,8 +0,0 @@
-const BannerTopzoneBanner = require("../assets/Images/BannerTopzone/banner-topzone.png")
-
-const BannerTopzone = {
-    id: 1,
-    image: BannerTopzoneBanner,
-}
-
-export default BannerTopzone;

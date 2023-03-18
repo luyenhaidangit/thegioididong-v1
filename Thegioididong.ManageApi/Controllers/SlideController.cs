20248247eeae336d33a7bd5febefc0fb9663e1a6
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-03-18 08:24:14 +0700

    Create API update, delete slide

--- Thegioididong.ManageApi/Controllers/SlideController.cs
+++ Thegioididong.ManageApi/Controllers/SlideController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Thegioididong.Model.Models;
+using Thegioididong.Model.ViewModels.Catalog.Products;
 using Thegioididong.Model.ViewModels.CMS.Slides;
 using Thegioididong.Model.ViewModels.Common;
 using Thegioididong.Service;
@@ -23,6 +24,51 @@ namespace Thegioididong.ManageApi.Controllers
         public PagedResult<SlideManageGetResult> Get([FromQuery] SlidePagingManageGetRequest request)
         {
             return _slideService.GetSlides(request);
+        }
+
+        [Route("Create")]
+        [HttpPost]
+        public ApiResult<string> Create([FromBody] SlideCreateRequest request)
+        {
+            try
+            {
+                bool result = _slideService.Create(request);
+                return new ApiSuccessResult<string>("Tạo slide thành công!");
+            }
+            catch (Exception ex)
+            {
+                return new ApiSuccessResult<string>("Tạo slide thất bại!");
+            }
+        }
+
+        [Route("Update")]
+        [HttpPut]
+        public ApiResult<string> Update([FromBody] SlideUpdateRequest request)
+        {
+            try
+            {
+                bool result = _slideService.Update(request);
+                return new ApiSuccessResult<string>("Cập nhật slide thành công!");
+            }
+            catch (Exception ex)
+            {
+                return new ApiSuccessResult<string>("Cập nhật slide thất bại!");
+            }
+        }
+
+        [Route("Delete")]
+        [HttpDelete]
+        public ApiResult<string> Delete([FromQuery] int id)
+        {
+            try
+            {
+                bool result = _slideService.Delete(id);
+                return new ApiSuccessResult<string>("Xóa slide thành công!");
+            }
+            catch (Exception ex)
+            {
+                return new ApiSuccessResult<string>("Xóa slide thất bại!");
+            }
         }
     }
 }

4492efff4800a848f7cc2271ee126cad95bf232d
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-04-01 23:44:26 +0700

    Modify model api result

--- Thegioididong.Model/ViewModels/Common/ApiResult.cs
+++ Thegioididong.Model/ViewModels/Common/ApiResult.cs
@@ -8,6 +8,18 @@ namespace Thegioididong.Model.ViewModels.Common
 {
     public class ApiResult<T>
     {
+        public ApiResult()
+        {
+
+        }
+
+        public ApiResult(int statusCode, string message, T data)
+        {
+            StatusCode = statusCode;
+            Message = message;
+            Data = data;
+        }
+
         public int StatusCode { get; set; }
 
         public string Message { get; set; }

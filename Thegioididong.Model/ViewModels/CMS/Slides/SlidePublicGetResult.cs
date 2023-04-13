1b80267f634b38ee472c490332145ee5529d6c4d
luyenhaidangit <106638147+luyenhaidangit@users.noreply.github.com>
2023-04-13 14:21:28 +0700

    Create api get slide shopping trends

--- Thegioididong.Model/ViewModels/CMS/Slides/SlidePublicGetResult.cs
+++ Thegioididong.Model/ViewModels/CMS/Slides/SlidePublicGetResult.cs
@@ -10,6 +10,8 @@ namespace Thegioididong.Model.ViewModels.CMS.Slides
     {
         public string Title { get; set; }
 
+        public string Description { get; set; }
+
         public string Image { get; set; }
 
         public string URL { get; set; }
